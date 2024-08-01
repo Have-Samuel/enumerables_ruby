29769d5b270af07be6aacb83427c0d1f9662dac3
samuel <samiehave9@gmail.com>
2024-08-01 11:01:00 +0300

    EACH Method

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-frds = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+frds = ['Alice', 'Bob', 'Charlie', 'David', 'Eve ---']
 invite_frds = []
 for friend in frds
   if friend != 'David'
@@ -6,3 +6,18 @@ for friend in frds
   end
 end
 puts invite_frds
+
+# Using select and reject methods
+frds = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+frds.select { |friend| friend != 'David' }
+frds.reject { |friend| friend == 'David' }
+puts frds
+
+# EACH METHOD
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+fruits.each { |fruit| puts "I Love all these #{fruit} fruits" }
+
+# OR
+fruits.each do |fruit|
+  print "I Love all these #{fruit} fruits"
+end
