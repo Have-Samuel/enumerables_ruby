348291c5827cd5959b28bf97cea6abcdcc4e0cb9
samuel <samiehave9@gmail.com>
2024-08-01 12:24:27 +0300

    Each method and gsub method

--- test.rb
+++ test.rb
@@ -42,14 +42,42 @@ end
 # Each Method is non-destructive, it doesn't change the original array or hash
 names = ['hugh', 'larry', 'moe', 'curly', 'shemp', 'joe']
 
-names.each { |name| puts name.upcase }
+caps = names.each { |name| name.upcase }
 
 puts names
+puts caps
+#=> ['hugh', 'larry', 'moe', 'curly', 'shemp', 'joe']
 
-# Each with Index
+# Each with Index, is non-destructive as well
 schools = ['Harvard', 'MIT', 'Stanford', 'Yale', 'Princeton']
 
 schools.each_with_index do |school, index|
   puts "#{school}" if index.odd?
 end
 
+# MAP METHOD
+# The map method is used to create a new array with the results of running a block once for every element in the array.
+# Modify the Each's examples of making the names all caps
+names = ['hugh', 'larry', 'moe', 'curly', 'shemp', 'joe']
+new_names = []
+names.map { |name| new_names.push(name.upcase)}
+puts "These are the new names in caps: #{new_names}"
+# p "These are the new names in caps: #{new_names}"
+# print "These are the new names in caps: #{new_names}"
+
+# OR
+names = ['hugh', 'larry', 'moe', 'curly', 'shemp', 'joe','jim']
+caps2 = names.map { |name| name.upcase }
+
+
+puts caps2
+puts names
+# Map! is destructive, it changes the original array
+
+# Map method with gsub method, to replace 'small' with 'big' in the classes array. And it's non-destructive
+classes = ['small class1', 'small class2', 'small class3', 'small class4', 'small class5']
+
+newer = classes.map { |item| item.gsub('small', 'big') }
+
+puts newer
+puts classes
