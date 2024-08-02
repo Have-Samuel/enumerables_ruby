c9b5646ebf9ec792e0d0ce61846564d48b9e2a20
samuel <samiehave9@gmail.com>
2024-08-02 09:27:30 +0300

    MAP without ! and MAP with !

---
+++ bang_mtd.rb
@@ -0,0 +1,14 @@
+friends = ['john', 'jane', 'joe', 'james',]
+
+caps1 = friends.each { |friend| friend.capitalize }
+puts caps1 # => Why doesn't the EACH method return the names in caps? Its 
+
+caps2 = friends.map { |friend| friend.upcase }
+
+puts caps2 # Why does the MAP method return caps? 
+puts friends
+# To change the original array, use the map! method
+caps3 = friends.map! { |friend| friend.upcase }
+
+puts caps3
+puts friends
