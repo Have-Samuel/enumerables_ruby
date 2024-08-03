e457832dc57681d31dc2d5d7b243b5d215848b3c
samuel <samiehave9@gmail.com>
2024-08-03 14:18:40 +0300

    Using the reduce samples

--- reduce_mtd.rb
+++ reduce_mtd.rb
@@ -46,3 +46,17 @@ puts output
 puts votes
 #=> {"Bob's Dirty Burger Shack"=>2, "St. Mark's Bistro"=>1}
 
+# What if we want to do something that keeps track of the result as we iterate? Like, say, summing up the elements of an array?
+# For that we need to use #inject (aka #reduce)
+people = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+puts people.reduce('') { |result, person| result + person} # => AliceBobCharlieDavidEve
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+puts array.inject(0) { |result, num| result + num } # => 153
+
+arr = [1, 2, 3, 4, 5]
+output = arr.inject(0) do |result, num|
+  result + num
+end
+
+puts output # => 15
